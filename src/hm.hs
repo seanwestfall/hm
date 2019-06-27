d3744ac403a277e56a1d53a928f246cc4cc34810
seanwestfall <sean.westfall@gmail.com>
2019-06-27 10:13:54 -0700

    Updated hm to a simple parser

--- src/hm.hs
+++ src/hm.hs
@@ -1,7 +1,18 @@
-module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
 main :: IO ()
-main = do
-   args <- getArgs
-   putStrLn ("Hello, " ++ args !! 0)
+main = do 
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
