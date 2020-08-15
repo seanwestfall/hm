a3c54926cec2b2eeb19e0941190879bfe1772fc5
seanwestfall <sean.westfall@gmail.com>
2020-08-14 22:15:18 -0700

    sw / getting started on the typechecker

---
+++ src/core/TypeChecker.hs
@@ -0,0 +1,11 @@
+module TypeChecker (typechecker) where
+
+import Core.Parsers
+import Core.Types
+import Core.Utils
+import Core.Environment
+import Core.ListPrimitives
+
+TypeChecker :: IO
+TypeChecker = do
+
